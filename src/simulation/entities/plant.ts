47351b7c61d486570f74fd6d60ebd28bf38998fa
BEKHEIRA Ryad <bekheiraryad@gmail.com>
2025-05-02 16:12:39 +0200

    commentaire partie plantes

--- src/simulation/entities/plant.ts
+++ src/simulation/entities/plant.ts
@@ -31,23 +31,25 @@ export default class Plant extends Entity {
 		this.replicate(replications);
   	}
 
-	
+	// Fonction pour répliquer (créer) une ou plusieurs nouvelles plantes
 	public replicate<T extends number = number>(amount: T = 1 as T): T extends 0 ? void : Plant {
 		if (amount > 0) {
 			let position: Vector;
 
+			// Cherche une position valide (à l'intérieur de la carte)
 			while (!position! || Simulation.instance.map.isOutside(position)) {
-				const angle = Simulation.instance.spawner.randomAngle();
-				const distance = Simulation.instance.spawner.randomInt(25, 350);
+				const angle = Simulation.instance.spawner.randomAngle(); // Angle aléatoire
+				const distance = Simulation.instance.spawner.randomInt(25, 350);// Distance aléatoire entre 25 et 350 unités
 
-				position = new Vector(angle, distance, true).add(this.position);
+				position = new Vector(angle, distance, true).add(this.position);  // Calcule la position autour de la plante actuelle
 			}
-
+			
+			// Crée une nouvelle plante à la position trouvée
 			const plant = Entity.create("plant", {
 				position,
 			}, amount - 1) as Plant;
 
-			return plant as any;
+			return plant as any; // Retourne la nouvelle plante
 		}
 
 		else {
